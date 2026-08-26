f4aadf62471590d03ee37522eaab677b7a35299e
muminovm <muminovxr@gmail.com>
2026-08-26 13:36:40 +0500

    Serve index.html via FastAPI

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Response, status
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "ok", "message": "SysAdmin API v2.0 is running!"}
+    return FileResponse("index.html")
 
 @app.get("/health")
 def health_check(response: Response):
-    # Если сервер работает нормально, возвращаем 200 OK
     response.status_code = status.HTTP_200_OK
     return {"status": "healthy"}
